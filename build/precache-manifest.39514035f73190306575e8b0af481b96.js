993e2685ff6a14a4d1381de311c62a4871a9eee2
Mari0203 <mari.brands@pm.me>
2019-09-07 17:54:32 -0500

    push

---
+++ build/precache-manifest.39514035f73190306575e8b0af481b96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fb6a34db8c8cf241b47dc2f5b5b358",
+    "url": "/Clickity-Click-Memory-Game/index.html"
+  },
+  {
+    "revision": "00482938dfddc9ada37e",
+    "url": "/Clickity-Click-Memory-Game/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "606b67ae68288e2d616e",
+    "url": "/Clickity-Click-Memory-Game/static/js/2.5f124323.chunk.js"
+  },
+  {
+    "revision": "00482938dfddc9ada37e",
+    "url": "/Clickity-Click-Memory-Game/static/js/main.39d99aa4.chunk.js"
+  },
+  {
+    "revision": "ea2c441223da1f73421b",
+    "url": "/Clickity-Click-Memory-Game/static/js/runtime~main.3df91d32.js"
+  }
+]);
