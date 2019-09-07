2af61841046453b66952952253bd7b720942e266
Mari0203 <mari.brands@pm.me>
2019-09-07 17:43:55 -0500

    Debugged score keepig loops

--- client/src/App.js
+++ client/src/App.js
@@ -8,39 +8,48 @@ class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     friends,
-    curretScore: 0,
+    currentScore: 0,
     highScore: 0
   };
 
   resetClicked = () => {
-    const newFriends = this.state.firneds.map(friend => {
+    const newArray = this.state.friends.map(friend => {
       friend.clicked = false;
       return friend;
     });
     this.setState({
-      friends: newFriends
+      friends: newArray
     });
   };
 
   removeFriend = id => {
     console.log("id: ", id);
 
-    const friends = this.state.friends.map(friend => {
+    const newArray = this.state.friends.map(friend => {
       if (id === friend.id) {
         // Executes below ONLY if friend.clicked is TRUE (i.e. it exists!):
         if (friend.clicked) {
           this.setState({
             currentScore: 0
           });
+          this.resetClicked();
 
-          // Executes below else function if friend.clicked is FALSE (i.e. it DOES NOT exists!)
+        // Executes below else function if friend.clicked is FALSE (i.e. it DOES NOT exists!)
         } else {
-          this.setState({
+
+          if (this.state.currentScore >= this.state.highScore) {
+            this.setState({
             // Update the scores by an increment of 1
-            currentScore: this.state.currentScore + 1,
-            highScore: this.state.highScore + 1
-          });
-        }
+              currentScore: this.state.currentScore + 1,
+              highScore: this.state.highScore + 1
+            });
+          } 
+          else {
+            this.setState({
+              currentScore: this.state.currentScore +1
+            });
+          }
+        };
 
         // If the clicked-friend(image) doesn't exist...:
         if (!friend.clicked) {
@@ -49,19 +58,28 @@ class App extends Component {
       }
 
       return friend;
+
     });
 
     console.log(friends);
 
     // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+    this.setState({ friends: newArray });
 
+    const newShuffledArray = this.state.friends.sort( () => Math.random() - .5);
+    
+    this.setState({
+      friends: newShuffledArray
+    });
+  }
   // Map over this.state.friends and render a FriendCard component for each friend object
-  render() {
+render() {
     return (
       <Wrapper>
         <Title>Friends List</Title>
+
+        Your Current Score: {this.state.currentScore} Your Best Score: {this.state.highScore} 
+
         {this.state.friends.map(friend => (
           <FriendCard
             removeFriend={this.removeFriend}
